2e9197a8d6cee0784c3077c6a496a9bbfd0601b6
Pulkit-Rana <pulkit.rana221@gmail.com>
2023-09-05 17:42:08 +0530

    Created pageobjects and fixture files.

--- cypress/e2e/1-getting-started/login.cy.js
+++ cypress/e2e/1-getting-started/login.cy.js
@@ -1,16 +1,23 @@
 /// <reference types="cypress" />
 
+import { LoginPage } from "../../support/pageobjects/loginpage"
+
+const loginpage = new LoginPage()
+
 describe("Tho test Login functionality and navigate to dashboard", () => {
     beforeEach(() => {
+        cy.fixture("/Loginpage.json").as("data")
         cy.visit("https://opensource-demo.orangehrmlive.com/web/")
     })
 
     it("Validate Login Functionality", () => {
-        cy.get(".orangehrm-login-container").should("be.visible")
-        cy.get('[placeholder="Username"]').type("Admin", { force: true })
-        cy.get('[placeholder="Password"]').type("admin123", { force: true })
-        cy.get('[type="submit"]').click({ force: true })
-        cy.get(".oxd-main-menu-item-wrapper").contains("Dashboard").should("have.class", "oxd-main-menu-item active")
-    })
+        cy.get("@data").then((data) => {
+            loginpage.getLoginContainerUI().should("be.visible")
+            loginpage.getUserName().type(data.userName, { force: true })
+            loginpage.getuserPassword().type(data.password, { force: true })
+            loginpage.getLoginSubmiButton().click({ force: true })
+            loginpage.getDashboardTab().contains("Dashboard").should("have.class", "oxd-main-menu-item active")
+        })
 
+    })
 })
---
+++ cypress/support/pageobjects/loginpage.js
@@ -0,0 +1,23 @@
+export class LoginPage {
+
+    getLoginContainerUI() {
+        return cy.get(".orangehrm-login-container")
+    }
+
+    getUserName() {
+        return cy.get('[placeholder="Username"]')
+    }
+
+    getuserPassword() {
+        return cy.get('[placeholder="Password"]')
+    }
+
+    getLoginSubmiButton() {
+        return cy.get('[type="submit"]')
+    }
+
+    getDashboardTab() {
+        return cy.get(".oxd-main-menu-item-wrapper")
+    }
+
+}
