64b958703730cb6784524a11e834d6ea254ee578
Pulkit-Rana <pulkit.rana221@gmail.com>
2023-09-12 16:38:58 +0530

    resolved merge conflict

--- cypress/e2e/1-getting-started/dashboard.cy.js
+++ cypress/e2e/1-getting-started/dashboard.cy.js
@@ -18,10 +18,6 @@ describe("Tho test Login functionality and navigate to dashboard", () => {
 
     it("Validating the tiles on the dashboard tab", () => {
         cy.get(".oxd-sheet.oxd-sheet--rounded.oxd-sheet--white.orangehrm-dashboard-widget").should("have.length", 7)
-<<<<<<< HEAD
-     
-        })
-=======
         cy.get(".oxd-grid-3.orangehrm-dashboard-grid").should("be.visible").then($ele => {
             expect($ele.text())
                 .include("Time at Work")
@@ -33,6 +29,5 @@ describe("Tho test Login functionality and navigate to dashboard", () => {
                 .and.include("Employee Distribution by Location")
         })
     })
->>>>>>> 59342886862fed9a507d6bbdaf6fdaeb418c2d4e
 })
 
