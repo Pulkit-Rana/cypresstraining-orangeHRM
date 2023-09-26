8ad47131838e7bd81f328ca8f59b3b3bb3e00276
Pulkit-Rana <pulkit.rana221@gmail.com>
2023-09-27 02:03:05 +0530

    cvonfigs

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: 'cypress-multi-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
-    baseUrl: "https://opensource-demo.orangehrmlive.com/",
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
+    viewportWidth:1920,
+    viewportHeight:1080,
+    baseUrl: "https://opensource-demo.orangehrmlive.com/",
   },
 });
